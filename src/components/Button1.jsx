409c3f90bf5828f6666258c1b703f20782485673
gksktl111 <gksktl111@naver.com>
2023-08-24 18:03:58 +0900

    추가

--- src/components/Button1.jsx
+++ src/components/Button1.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './Button1.module.css';
 
 export default function Button1() {
-  return <div className={styles.button}>Button1</div>;
+  return <div className={styles.button}>Button1-멋져!!</div>;
 }
