ced75ec56a30df719cfaf2e98d72a3404126170b
gksktl111 <gksktl111@naver.com>
2023-08-24 17:54:31 +0900

    추가

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+  return (
+    <div className='bg-white dark:bg-slate-800'>
+      <h1 className='text-8xl'>안녕!</h1>
+      <button className='bg-blue-400 rounded-xl px-5 font-bold'>Button</button>
+    </div>
+  );
+}
